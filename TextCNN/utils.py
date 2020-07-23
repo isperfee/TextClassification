4f3840f3f2ae17f00e38a659adf24f6491e40f4d
pantheonzhang <zhangpengfei2013@qq.com>
2020-07-23 15:02:43 +0800

    remove temp model file

--- TextCNN/utils.py
+++ TextCNN/utils.py
@@ -93,17 +93,3 @@ def process_file(file_name, word_to_id, cat_to_id, max_length=600):
     y_pad = to_categorical(label_id, num_classes=len(cat_to_id))
 
     return x_pad, y_pad
-
-
-def batch_iter(x, y, batch_size=64):
-    data_len = len(x)
-    num_batch = int((data_len - 1) / batch_size) + 1
-
-    indices = np.random.permutation(np.arange(data_len))
-    x_shuffle = x[indices]
-    y_shuffle = y[indices]
-
-    for i in range(num_batch):
-        start_id = i * batch_size
-        end_id = min((i + 1) * batch_size, data_len)
-        yield x_shuffle[start_id:end_id], y_shuffle[start_id:end_id]
